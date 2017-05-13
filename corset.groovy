0ce35548aa3333757af9d395ac7621bd86a377bf
Kevin Harrington <mad.hephaestus@gmail.com>
2017-05-13 16:33:37 -0400

    adding measurments

--- corset.groovy
+++ corset.groovy
@@ -1,1 +1,14 @@
-//Your code here
+// horizontal
+bustSize 		= new LengthParameter("Bust Size",30,[120.0,1.0])
+underbust		= new LengthParameter("underbust",30,[120.0,1.0])
+waist 		= new LengthParameter("waist",30,[120.0,1.0])
+highHip		= new LengthParameter("high hip",30,[120.0,1.0])
+lowHip 		= new LengthParameter("low hip",30,[120.0,1.0])
+// verticals
+upBreast 		= new LengthParameter("up breast",30,[120.0,1.0])
+downbreast	= new LengthParameter("down breast",30,[120.0,1.0])
+midBreast		= new LengthParameter("middle of breast",30,[120.0,1.0])
+uBreastToWaist	= new LengthParameter("under breast to waist",30,[120.0,1.0])
+waistToPubic 	= new LengthParameter("waist to pubic bone",30,[120.0,1.0])
+waistHighHip	= new LengthParameter("waist high hip",30,[120.0,1.0])
+waistLowHip	= new LengthParameter("waist low hip",30,[120.0,1.0])
