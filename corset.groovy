b9e0a82ef1199175b8718d55fb6dc781ab438810
Kevin Harrington <mad.hephaestus@gmail.com>
2017-12-26 18:17:11 -0500

    progress on path generation with 6 control measurment points

--- corset.groovy
+++ corset.groovy
@@ -20,8 +20,23 @@ waistHighHip	= new LengthParameter("waist high hip",mm(4),[120.0,1.0])
 waistBackTop	= new LengthParameter("waist to top back",mm(8),[120.0,1.0])
 waistBackBottom= new LengthParameter("waist to bottom back",mm(7.5),[120.0,1.0])
 // construction
-numPanels	= new LengthParameter("number of panels",8,[12,4])
-
+numPanels	= new LengthParameter("number of panels",4,[12,4])
+static ArrayList<Line3D> showEdges(ArrayList<Vector3d> finalPath,Double offset, javafx.scene.paint.Color color ){
+	 ArrayList<Line3D> lines =[]
+	for(int i=0;i<finalPath.size();i++){
+		
+		double z=offset
+		Vector3d p1 =finalPath.get(i)
+		Vector3d p2 = finalPath.get(i<(finalPath.size()-1)?i+1:0)
+		Line3D line = new Line3D(p1,p2);
+		line.setStrokeWidth(0.8);
+		line.setStroke(color);
+		lines .add(line);
+		BowlerStudioController.getBowlerStudio() .addNode(line)
+	}
+	
+	return lines
+}
 public static CSG byPath(List<List<Vector3d>> points, double height) {
 
 	return byPath(points, height, 200);
@@ -30,13 +45,17 @@ public static CSG byPath(List<List<Vector3d>> points, double height) {
 
 public static CSG byPath(List<List<Vector3d>> points, double height, int resolution) {
 	ArrayList<Transform> trPath = pathToTransforms(points, resolution);
-	List<Vector3d> finalPath = new ArrayList<>();
+	ArrayList<Vector3d> finalPath = new ArrayList<>();
 	for (Transform tr : trPath) {
 		javax.vecmath.Vector3d t1 = new javax.vecmath.Vector3d();
 		tr.getInternalMatrix().get(t1);
 		Vector3d finalPoint = new Vector3d(t1.x , t1.y , 0);
 		finalPath.add(finalPoint);
 	}
+	showEdges(finalPath,(double)0.0,javafx.scene.paint.Color.RED)
+	//List<Polygon> p =  Polygon.fromConcavePoints(finalPath)
+	//for(Polygon pl:p)
+	//	BowlerStudioController.getBowlerStudio()addObject(pl,null)
 	return Extrude.points(new Vector3d(0, 0, height), finalPath);
 }
 public static ArrayList<Transform> pathToTransforms(List<List<Vector3d>> points, int resolution){
@@ -76,8 +95,8 @@ public static ArrayList<Transform> pathToTransforms(List<List<Vector3d>> points,
 			//start.set(2, sections.get(0).z);
 		}
 	}
-	println "A string = " +pathStringA
-	println "B String = " +pathStringB
+	//println "A string = " +pathStringA
+	//println "B String = " +pathStringB
 	BezierPath path = new BezierPath();
 	path.parsePathString(pathStringA);
 	BezierPath path2 = new BezierPath();
@@ -135,63 +154,82 @@ public static CSG profileWithHoles(List<List<Vector3d>> profile){
 
 	return shape
 }
+
 ArrayList<CSG> panels=[]
 int panelsPerSide = numPanels.getMM()/2
 double panelMaxWidth = lowHip.getMM()/numPanels.getMM()
 double waistSecion = waist.getMM()/numPanels.getMM()
 double widthDifference = (panelMaxWidth-waistSecion)/2
-widthDifference=widthDifference-(widthDifference/numPanels.getMM())
+widthDifference=widthDifference+(widthDifference/numPanels.getMM())
+double MinHeightUpper = uBreastToWaist.getMM()
+double MaxHeightUpper =waistBackTop.getMM()
+double MinHeightLower = waistHighHip.getMM()
+double MaxHeightLower =waistBackBottom.getMM()
 
 for(int i=0;i<panelsPerSide;i++){
-
-	double heightRight = waistHighHip.getMM()+uBreastToWaist.getMM()
-	double heightLeft = waistHighHip.getMM()+uBreastToWaist.getMM()
+	double incrementA = Math.sin(Math.PI*(i)/panelsPerSide)
+	double incrementB = Math.sin(Math.PI*(i+1)/panelsPerSide)
+	println "Increment A = "+incrementA+" increment b "+incrementB
+	double heightDifferenceUpper =MaxHeightUpper- MinHeightUpper
+	double heightDifferenceLower =MaxHeightLower- MinHeightLower
 	
-	double controlOffsetRight = heightRight/4
-	double controlOffsetLeft  = heightRight/4
-	List<Vector3d> rightSideUpper=[	new Vector3d(0,0,0),
-				new Vector3d(0,controlOffsetRight,0),
+	double heightRightUpper = -(MaxHeightUpper-(heightDifferenceUpper*incrementA))
+	double heightLeftUpper = -(MaxHeightUpper-(heightDifferenceUpper*incrementB))
+	double heightRightLower = MaxHeightLower-(heightDifferenceLower*incrementA)
+	double heightLeftLower = MaxHeightLower-(heightDifferenceLower*incrementB)
+	
+	double controlOffsetRight = MaxHeightLower/4
+	double controlOffsetLeft  = MaxHeightLower/4
+
+	Vector3d upperRight = new Vector3d(0,heightRightUpper,0)
+	Vector3d centerRight = new Vector3d(widthDifference,0,0)
+	Vector3d bottomRight = new Vector3d(0,heightRightLower,0)
+	Vector3d bottomLeft  = new Vector3d(panelMaxWidth,heightLeftLower,0)
+	Vector3d centerLeft = new Vector3d(panelMaxWidth-widthDifference,0,0)
+	Vector3d upperleft = new Vector3d(panelMaxWidth,heightLeftUpper,0)
+	List<Vector3d> rightSideUpper=[	upperRight,
+				new Vector3d(0,heightRightUpper+controlOffsetRight,0),
+				new Vector3d(widthDifference,-controlOffsetRight ,0),
+				centerRight]
+	List<Vector3d> rightSideLower=[	centerRight,
 				new Vector3d(widthDifference,controlOffsetRight ,0),
-				new Vector3d(widthDifference,heightRight/2,0)]
-	List<Vector3d> rightSideLower=[	new Vector3d(widthDifference,heightRight/2,0),
-				new Vector3d(widthDifference,heightRight/2+controlOffsetRight ,0),
-				new Vector3d(0,heightRight-controlOffsetRight ,0),
-				new Vector3d(0,heightRight,0)]
-	List<Vector3d> bottom =[ new Vector3d(0,heightRight,0),
-				new Vector3d(10,heightRight-10,0),
-				new Vector3d(panelMaxWidth-10,heightLeft-10,0),
-				new Vector3d(panelMaxWidth,heightLeft,0)]
-	List<Vector3d> leftSideLower =[new Vector3d(panelMaxWidth,heightLeft,0),
-				new Vector3d(panelMaxWidth,heightLeft-controlOffsetLeft,0),
-				new Vector3d(panelMaxWidth-widthDifference,heightLeft/2+controlOffsetLeft,0),
-				new Vector3d(panelMaxWidth-widthDifference,heightLeft/2,0)]
-	List<Vector3d> leftSideUpper =[new Vector3d(panelMaxWidth-widthDifference,heightLeft/2,0),
+				new Vector3d(0,heightRightLower-controlOffsetRight ,0),
+				bottomRight]
+	List<Vector3d> bottom =[ bottomRight,
+				new Vector3d(10,heightRightLower,0),
+				new Vector3d(panelMaxWidth-10,heightLeftLower,0),
+				bottomLeft]
+	List<Vector3d> leftSideLower =[bottomLeft,
+				new Vector3d(panelMaxWidth,heightLeftLower-controlOffsetLeft,0),
 				new Vector3d(panelMaxWidth-widthDifference,controlOffsetLeft,0),
-				new Vector3d(panelMaxWidth,controlOffsetLeft,0),
-				new Vector3d(panelMaxWidth,0,0)]
-	List<Vector3d> top =[	new Vector3d(panelMaxWidth,0,0),
-				new Vector3d(panelMaxWidth,0,0),
-				new Vector3d(0,0,0),
-				new Vector3d(0,0,0)]	
+				centerLeft]
+	List<Vector3d> leftSideUpper =[centerLeft,
+				new Vector3d(panelMaxWidth-widthDifference,-controlOffsetLeft,0),
+				new Vector3d(panelMaxWidth,heightLeftUpper+controlOffsetLeft,0),
+				upperleft]
+	List<Vector3d> top =[	upperleft,
+				new Vector3d(panelMaxWidth-10,heightLeftUpper,0),
+				new Vector3d(10,heightRightUpper,0),
+				upperRight]	
 	if(i==(panelsPerSide-1)){
-		leftSideLower =[new Vector3d(panelMaxWidth,heightLeft,0),
-				new Vector3d(panelMaxWidth,heightLeft,0),
-				new Vector3d(panelMaxWidth,heightLeft/2,0),
-				new Vector3d(panelMaxWidth,heightLeft/2,0)]
-		leftSideUpper =[new Vector3d(panelMaxWidth,heightLeft/2,0),
-				new Vector3d(panelMaxWidth,heightLeft/2,0),
+		leftSideLower =[bottomLeft,
+				bottomLeft,
 				new Vector3d(panelMaxWidth,0,0),
 				new Vector3d(panelMaxWidth,0,0)]
+		leftSideUpper =[new Vector3d(panelMaxWidth,0,0),
+				new Vector3d(panelMaxWidth,0,0),
+				upperleft,
+				upperleft]
 	}
 	if(i==0){
-		rightSideUpper=[	new Vector3d(0,0,0),
+		rightSideUpper=[	upperRight,
+				upperRight,
 				new Vector3d(0,0,0),
-				new Vector3d(0,heightRight/2,0),
-				new Vector3d(0,heightRight/2,0)]
-		rightSideLower=[	new Vector3d(0,heightRight/2,0),
-				new Vector3d(0,heightRight/2,0),
-				new Vector3d(0,heightRight,0),
-				new Vector3d(0,heightRight,0)]
+				new Vector3d(0,0,0)]
+		rightSideLower=[	new Vector3d(0,0,0),
+				new Vector3d(0,0,0),
+				bottomRight,
+				bottomRight]
 	}
 			
 	List<List<Vector3d>>  profile = [
@@ -202,17 +240,43 @@ for(int i=0;i<panelsPerSide;i++){
 			leftSideUpper,
 			top
 	]
-	
+	/*
+	profile = [
+			[upperRight],
+			[centerRight],
+			[bottomRight],
+			[bottomLeft],
+			[centerLeft],
+			[upperleft],
+			[upperRight]
+	]
+	*/
+	println profile
 	CSG shape = byPath(profile,5)
-	CSG hole =  new Cube(1,1,30).toCSG()
+	//CSG shape = new Cube(1).toCSG()
+	CSG holeR =  new Cube(1,1,30).toCSG()
 					.movey(-3)
-
+	CSG holeL =  new Cube(1,1,30).toCSG()
+					.movey(-3)
+	if(i==0){
+		holeR =  new Cylinder(2,30).toCSG()
+					.movey(-5)					
+					.movez(-15)
+	}
+	if(i==(panelsPerSide-1)){
+		holeL =  new Cylinder(2,30).toCSG()
+					.movey(-5)
+					.movez(-15)
+	}
 	//holeParts.remove(holeParts.size()-1)
-	shape=shape.difference( Extrude.move(hole,bezierToTransforms(rightSideUpper,  15)))
-			 .difference( Extrude.move(hole,bezierToTransforms(rightSideLower,  15)))
-			 .difference( Extrude.move(hole,bezierToTransforms(leftSideUpper,  15)))
-			 .difference( Extrude.move(hole,bezierToTransforms(leftSideLower,  15)))
-			 .movex((i*panelMaxWidth)+ (10*i))
+	shape=shape.union( Extrude.move(holeR,bezierToTransforms(rightSideUpper,  15)))
+			 .union( Extrude.move(holeR,bezierToTransforms(rightSideLower,  25)))
+			 .union( Extrude.move(holeL,bezierToTransforms(leftSideUpper,  15)))
+			 .union( Extrude.move(holeL,bezierToTransforms(leftSideLower,  15)))
+	if(i==(panelsPerSide-1))
+		shape=shape .movex((-panelMaxWidth)- (10))
+	else
+		shape=shape.movex((i*panelMaxWidth)+ (20*i))
 	/*
 	cornerOne = new Vector3d(0,-waistBackBottom.getMM(),0)
 	cornerTwo = new Vector3d(0,waistBackTop.getMM(),0)
